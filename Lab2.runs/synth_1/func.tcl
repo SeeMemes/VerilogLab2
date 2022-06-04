c03e0e1351973beaa994b6fc5d2995113db28eeb
SeeMemes <provotorov-02@mail.ru>
2022-06-04 16:12:52 +0300

    v2.0

--- Lab2.runs/synth_1/func.tcl
+++ Lab2.runs/synth_1/func.tcl
@@ -22,16 +22,16 @@ create_project -in_memory -part xc7vx485tffg1157-1
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/User/Desktop/Verilog_Labs/VerilogLab2/Lab2.cache/wt [current_project]
-set_property parent.project_path C:/Users/User/Desktop/Verilog_Labs/VerilogLab2/Lab2.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/Elite/Desktop/1/VerilogLab2/Lab2.cache/wt [current_project]
+set_property parent.project_path C:/Users/Elite/Desktop/1/VerilogLab2/Lab2.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo c:/Users/User/Desktop/Verilog_Labs/VerilogLab2/Lab2.cache/ip [current_project]
+set_property ip_output_repo c:/Users/Elite/Desktop/1/VerilogLab2/Lab2.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 read_verilog -library xil_defaultlib {
-  C:/Users/User/Desktop/Verilog_Labs/VerilogLab2/Lab2.srcs/sources_1/new/mult.v
-  C:/Users/User/Desktop/Verilog_Labs/VerilogLab2/Lab2.srcs/sources_1/new/sqrt.v
-  C:/Users/User/Desktop/Verilog_Labs/VerilogLab2/Lab2.srcs/sources_1/new/func.v
+  C:/Users/Elite/Desktop/1/VerilogLab2/Lab2.srcs/sources_1/new/mult.v
+  C:/Users/Elite/Desktop/1/VerilogLab2/Lab2.srcs/sources_1/new/sqrt.v
+  C:/Users/Elite/Desktop/1/VerilogLab2/Lab2.srcs/sources_1/new/func.v
 }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -41,7 +41,7 @@ read_verilog -library xil_defaultlib {
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-set_param ips.enableIPCacheLiteLoad 0
+set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
 
 synth_design -top func -part xc7vx485tffg1157-1
